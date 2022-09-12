244747958e0ec42bcdb8ed232f904710e61150a8
lingdocs <71590811+lingdocs@users.noreply.github.com>
2022-09-12 19:15:19 +0400

    oops

--- src/library.ts
+++ src/library.ts
@@ -39,7 +39,7 @@ import Block, { NPBlock, APBlock } from "./components/blocks/Block";
 import { roleIcon } from "./components/vp-explorer/VPExplorerExplanationModal";
 import CompiledPTextDisplay from "./components/CompiledPTextDisplay";
 import RenderedBlocksDisplay from "./components/RenderedBlocksDisplay";
-import { getInflectionPattern } from "./lib/inflection-category";
+import { getInflectionPattern } from "./lib/inflection-pattern";
 import {
     makePsString,
     removeFVarients,
