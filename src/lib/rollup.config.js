df59113eb5945eeb32add00815bab8dea817b1ae
adueck <adueck@users.noreply.github.com>
2022-10-08 23:02:55 +0500

    mm

--- src/lib/rollup.config.js
+++ src/lib/rollup.config.js
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import pkg from './lib/package.json';
+import pkg from './package.json';
 
 const banner = `
 /**
@@ -13,9 +13,9 @@ const banner = `
 `;
 
 export default {
-  input: 'lib/dist/lib/library.js',
+  input: 'dist/lib/library.js',
   output: [{
-    file: "lib/dist/index.js",
+    file: "dist/index.js",
     format: 'cjs',
     sourcemap: true,
     banner,
