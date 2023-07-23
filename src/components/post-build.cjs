21a7a76ed26e2688563a98a7d69f104e4866231c
adueck <adueck@users.noreply.github.com>
2023-07-23 10:43:10 +0400

    css packaging

--- src/components/post-build.cjs
+++ src/components/post-build.cjs
@@ -3,20 +3,26 @@ const fs = require("fs-extra");
 const path = require("path");
 
 glob("src/verb-info/*.svg", (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-        fs.copySync(file, `dist/components/src/verb-info/${path.basename(file)}`);
-    });
+  if (err) throw err;
+  files.forEach((file) => {
+    fs.copySync(file, `dist/components/src/verb-info/${path.basename(file)}`);
+  });
 });
 glob("src/vp-explorer/*.svg", (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-        fs.copySync(file, `dist/components/src/vp-explorer/${path.basename(file)}`);
-    });
+  if (err) throw err;
+  files.forEach((file) => {
+    fs.copySync(file, `dist/components/src/vp-explorer/${path.basename(file)}`);
+  });
 });
 glob("src/vp-explorer/*.jpg", (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-        fs.copySync(file, `dist/components/src/vp-explorer/${path.basename(file)}`);
-    });
+  if (err) throw err;
+  files.forEach((file) => {
+    fs.copySync(file, `dist/components/src/vp-explorer/${path.basename(file)}`);
+  });
+});
+glob("src/vp-explorer/*.css", (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    fs.copySync(file, `dist/components/src/vp-explorer/${path.basename(file)}`);
+  });
 });
