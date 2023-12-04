5190cf2adbf48c5ec73017a13a3fe3d44c36a63f
adueck <adueck@users.noreply.github.com>
2023-12-04 14:11:23 +0400

    type error

--- src/lib/src/protobuf.ts
+++ src/lib/src/protobuf.ts
@@ -4,27 +4,31 @@ import { Dictionary, DictionaryInfo } from "./dictionary-models";
 import Pbf from "pbf";
 
 export function writeDictionary(dictionary: T.Dictionary): ArrayBuffer {
-    const pbfDict = new Pbf();
-    Dictionary.write(dictionary, pbfDict);
-    const buffer = pbfDict.finish();
-    return buffer;
+  const pbfDict = new Pbf();
+  // @ts-ignore
+  Dictionary.write(dictionary, pbfDict);
+  const buffer = pbfDict.finish();
+  return buffer;
 }
 
 export function readDictionary(buffer: Uint8Array): T.Dictionary {
-    const pbf = new Pbf(buffer);
-    const dictionary = Dictionary.read(pbf) as T.Dictionary;
-    return dictionary;
+  const pbf = new Pbf(buffer);
+  // @ts-ignore
+  const dictionary = Dictionary.read(pbf) as T.Dictionary;
+  return dictionary;
 }
 
 export function writeDictionaryInfo(dictionary: T.DictionaryInfo): ArrayBuffer {
-    const pbfDict = new Pbf();
-    DictionaryInfo.write(dictionary, pbfDict);
-    const buffer = pbfDict.finish();
-    return buffer;
+  const pbfDict = new Pbf();
+  // @ts-ignore
+  DictionaryInfo.write(dictionary, pbfDict);
+  const buffer = pbfDict.finish();
+  return buffer;
 }
 
 export function readDictionaryInfo(buffer: Uint8Array): T.DictionaryInfo {
-    const pbf = new Pbf(buffer);
-    const dictionaryInfo = DictionaryInfo.read(pbf) as T.DictionaryInfo;
-    return dictionaryInfo;
+  const pbf = new Pbf(buffer);
+  // @ts-ignore
+  const dictionaryInfo = DictionaryInfo.read(pbf) as T.DictionaryInfo;
+  return dictionaryInfo;
 }
