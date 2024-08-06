b2f3dfe93c3db4db9f7605d081a5164429936a20
adueck <adueck@users.noreply.github.com>
2024-08-06 13:29:50 -0400

    fixed phonetics on inflection patterns

--- src/components/src/HumanReadableInflectionPattern.tsx
+++ src/components/src/HumanReadableInflectionPattern.tsx
@@ -1,18 +1,30 @@
 import * as T from "../../types";
 import InlinePs from "./InlinePs";
 
-export default function HumanReadableInflectionPattern(p: T.InflectionPattern, textOptions: T.TextOptions): JSX.Element | null {
-    return p === 1
-        ? <span>#1 Basic</span>
-        : p === 2
-        ? <span>#2 Unstressed <InlinePs opts={textOptions}>{{ p: "ی", f: "ey", e: "" }}</InlinePs></span>
-        : p === 3
-        ? <span>#3 Stressed <InlinePs opts={textOptions}>{{ p: "ی", f: "éy", e: "" }}</InlinePs></span>
-        : p === 4
-        ? <span>#4 "Pashtoon"</span>
-        : p === 5
-        ? <span>#5 Short Squish</span>
-        : p === 6
-        ? <span>#6 Fem. inan. <InlinePs opts={textOptions}>{{ p: "ي", f: "ee", e: "" }}</InlinePs></span>
-        : null;
+export default function HumanReadableInflectionPattern(
+  p: T.InflectionPattern,
+  textOptions: T.TextOptions
+): JSX.Element | null {
+  return p === 1 ? (
+    <span>#1 Basic</span>
+  ) : p === 2 ? (
+    <span>
+      #2 Unstressed{" "}
+      <InlinePs opts={textOptions}>{{ p: "ی", f: "ay", e: "" }}</InlinePs>
+    </span>
+  ) : p === 3 ? (
+    <span>
+      #3 Stressed{" "}
+      <InlinePs opts={textOptions}>{{ p: "ی", f: "áy", e: "" }}</InlinePs>
+    </span>
+  ) : p === 4 ? (
+    <span>#4 "Pashtoon"</span>
+  ) : p === 5 ? (
+    <span>#5 Short Squish</span>
+  ) : p === 6 ? (
+    <span>
+      #6 Fem. inan.{" "}
+      <InlinePs opts={textOptions}>{{ p: "ي", f: "ee", e: "" }}</InlinePs>
+    </span>
+  ) : null;
 }
