1295ff05cfe190ba7f19d2f37342b8b54eeba163
adueck <adueck@users.noreply.github.com>
2024-08-06 17:39:59 -0400

    fix ci

--- get-words.js
+++ get-words.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 lingdocs.com
+ * Copyright (c) 2024 lingdocs.com
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -13,27 +13,37 @@ const nounAdjCollectionPath = path.join(".", "vocab", "nouns-adjs");
 const verbTsFiles = fs.readdirSync(verbCollectionPath);
 const nounAdjTsFiles = fs.readdirSync(nounAdjCollectionPath);
 
-const allVerbTsS = [...new Set(verbTsFiles.reduce((arr, fileName) => {
-    const TsS = require("./vocab/verbs/"+fileName);
-    return [...arr, ...TsS];
-}, []))];
+const allVerbTsS = [
+  ...new Set(
+    verbTsFiles.reduce((arr, fileName) => {
+      const TsS = require("./vocab/verbs/" + fileName);
+      return [...arr, ...TsS];
+    }, [])
+  ),
+];
 
-const allNounAdjTsS = [...new Set(nounAdjTsFiles.reduce((arr, fileName) => {
-    const TsS = require("./vocab/nouns-adjs/"+fileName).map(x => x.ts);
-    return [...arr, ...TsS];
-}, []))];
+const allNounAdjTsS = [
+  ...new Set(
+    nounAdjTsFiles.reduce((arr, fileName) => {
+      const TsS = require("./vocab/nouns-adjs/" + fileName).map((x) => x.ts);
+      return [...arr, ...TsS];
+    }, [])
+  ),
+];
 
 fetch("https://account.lingdocs.com/dictionary/entries", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ ids: [...allNounAdjTsS, ...allVerbTsS] }),
-}).then(res => res.json()).then(res => {
-  const verbs = res.results.filter(x => "entry" in x);
-  const verbsContent = `
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ ids: [...allNounAdjTsS, ...allVerbTsS] }),
+})
+  .then((res) => res.json())
+  .then((res) => {
+    const verbs = res.results.filter((x) => "entry" in x);
+    const verbsContent = `
 /**
- * Copyright (c) 2021 lingdocs.com
+ * Copyright (c) 2024 lingdocs.com
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -47,11 +57,11 @@ const verbs: {
     complement?: DictionaryEntry,
 }[] = ${JSON.stringify(verbs)};
 export default verbs as VerbEntry[];`;
-  fs.writeFileSync("./src/verbs.ts", verbsContent);
-  const nounsAdjs = res.results.filter(x => !("entry" in x));
-  const nounsAdjsContent = `
+    fs.writeFileSync("./src/verbs.ts", verbsContent);
+    const nounsAdjs = res.results.filter((x) => !("entry" in x));
+    const nounsAdjsContent = `
 /**
- * Copyright (c) 2021 lingdocs.com
+ * Copyright (c) 2024 lingdocs.com
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -62,16 +72,15 @@ import { DictionaryEntry } from "./types";
 
 const nounsAdjs: DictionaryEntry[] = ${JSON.stringify(nounsAdjs)};
 export default nounsAdjs;`;
-  fs.writeFileSync("./src/nouns-adjs.ts", nounsAdjsContent);
-  console.log("fetched words from dictionary");
-  const missingEc = res.results.filter(x => "entry" in x && !x.entry.ec);
-  if (missingEc.length) {
-    console.log("verbs missing ec");
-    console.log(missingEc);
-  }
-  if (res.notFound.length) {
-    console.log("entries not found:");
-    console.log(res.notFound);
-  }
-});
-
+    fs.writeFileSync("./src/nouns-adjs.ts", nounsAdjsContent);
+    console.log("fetched words from dictionary");
+    const missingEc = res.results.filter((x) => "entry" in x && !x.entry.ec);
+    if (missingEc.length) {
+      console.log("verbs missing ec");
+      console.log(missingEc);
+    }
+    if (res.notFound.length) {
+      console.log("entries not found:");
+      console.log(res.notFound);
+    }
+  });
